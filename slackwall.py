fa69909e54841acf7b140542c1df0daacc4a4862
meta-l <shrozzle@gmail.com>
2016-07-25 16:20:22 +0100

    Add files via upload

--- slackwall.py
+++ slackwall.py
@@ -3,6 +3,7 @@
 # Version 0.9
 # Licence: WTFPL - wtfpl.net
 # thanks to darren.martyn@xiphosrearch.co.uk for python advice and Mike Kemp @ Xiphos for general encouragement.
+# thanks also to @pelicancoder for code sanity
 
 import subprocess
 import sys
